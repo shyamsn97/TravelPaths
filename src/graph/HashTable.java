8c53481ce2b114a96b4dbe3f0d301f00aa1e52fa
Shyam <shyam.sudhakaran97@gmail.com>
2018-04-22 13:22:08 -0700

    added get and add method, room for modifications

--- src/graph/HashTable.java
+++ src/graph/HashTable.java
@@ -9,6 +9,52 @@ public class HashTable {
     public HashObject[] hasharray;
     public ArrayList<String> keys; //will probably change
 
+    public HashTable(int length) {
+        this.hasharray = new HashObject[length];
+    }
+
+    public int createHash(String key,int a) {
+        int hashvalue = 0;
+        double power = key.length() - 1;
+        double constant = 0;
+        for (int i = 0; i < key.length(); i++) {
+            constant = Math.pow(a,power);
+            power--;
+            hashvalue += (int) (constant * (int) key.charAt(i));
+        }
+        return hashvalue % hasharray.length;
+    }
+
+    public void add(String inputKey,int value) {
+
+        int place = createHash(inputKey,37);
+        HashObject newobj = new HashObject(inputKey,value);
+        if (hasharray[place] == null) {
+            hasharray[place] = newobj;
+        }
+        else {
+            HashObject current = hasharray[place];
+            while(current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newobj);
+        }
+    }
+
+    public int get(String inputKey) {
+
+        int place = createHash(inputKey,37);
+
+        HashObject placeobj = hasharray[place];
+        while(placeobj != null) {
+            if (placeobj.getKey().equals(inputKey)) {
+                return placeobj.getValue();
+            }
+        }
+        
+        return 0;
+    }
+
     public void readFile(String filename) {
         keys = new ArrayList<String>();
         BufferedReader bufferedReader = null;
@@ -39,4 +85,27 @@ class HashObject {
     private String key; // id of the neighbor ("destination" vertex of this edge)
     private int value; // cost of this edge
     private HashObject next; // reference to the next "edge" in the linked list
+
+    HashObject(String string, int value) {
+        key = string;
+        value = value;
+        next = null;
+    }
+
+    public void setNext(HashObject object) {
+        this.next = object;
+    }
+
+    public HashObject getNext() {
+        return next;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
 }
