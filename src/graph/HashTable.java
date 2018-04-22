90fcff228284caf8eaede8a852d5f5552453d9c7
Shyam <shyam.sudhakaran97@gmail.com>
2018-04-21 18:20:32 -0700

    Added hashtable node for the array of linked lists

--- src/graph/HashTable.java
+++ src/graph/HashTable.java
@@ -3,8 +3,14 @@ package graph;
 /** Custom implementation of a hash table using open hashing, separate chaining.
  *  Each key is a String (name of the city), each value is an integer (node id). */
 public class HashTable {
-	 // FILL IN CODE: add variables and methods
 
+    public HashObject[] hasharray;
+    
 
+}
 
+class HashObject {
+    private String key; // id of the neighbor ("destination" vertex of this edge)
+    private int value; // cost of this edge
+    private HashObject next; // reference to the next "edge" in the linked list
 }
