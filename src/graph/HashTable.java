18329c21636418e0b0c5d6765d03856ca3638408
Shyam <shyam.sudhakaran97@gmail.com>
2018-04-21 23:47:53 -0700

    Added file read in, reads into an array list, will probably change that

--- src/graph/HashTable.java
+++ src/graph/HashTable.java
@@ -1,12 +1,38 @@
 package graph;
+import java.util.ArrayList;
+import java.io.*;
 
 /** Custom implementation of a hash table using open hashing, separate chaining.
  *  Each key is a String (name of the city), each value is an integer (node id). */
 public class HashTable {
 
     public HashObject[] hasharray;
-    
+    public ArrayList<String> keys; //will probably change
 
+    public void readFile(String filename) {
+        keys = new ArrayList<String>();
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(filename));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String line = null;
+        try {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (line != null && line.length() > 0) {
+            keys.add(line);
+            try {
+                line = bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
 }
 
 class HashObject {
